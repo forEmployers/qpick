aa58b52f9c8403aa46489db023f37444536b6c37
Attila Meszaros <hello@maxking.com.au>
2020-08-16 20:03:42 +1000

    WP plugin added

---
+++ WP/precache-manifest.c8796aaa6ce63bbe7e1852ec74e60dd4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54cf8c719d1f65ae4be4361ac70677de",
+    "url": "./wp-content/plugins/quickpick/index.html"
+  },
+  {
+    "revision": "afb829f2c8d89f826243",
+    "url": "./wp-content/plugins/quickpick/static/css/2.ee0ceef3.chunk.css"
+  },
+  {
+    "revision": "43635e776fd2f8b0b3f4",
+    "url": "./wp-content/plugins/quickpick/static/css/main.2fdbc1bc.chunk.css"
+  },
+  {
+    "revision": "afb829f2c8d89f826243",
+    "url": "./wp-content/plugins/quickpick/static/js/2.07d15fad.chunk.js"
+  },
+  {
+    "revision": "2a5bf06f1944b65c2267e2af2244f675",
+    "url": "./wp-content/plugins/quickpick/static/js/2.07d15fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43635e776fd2f8b0b3f4",
+    "url": "./wp-content/plugins/quickpick/static/js/main.4827e727.chunk.js"
+  },
+  {
+    "revision": "718ce4aab1d5bf61df4a",
+    "url": "./wp-content/plugins/quickpick/static/js/runtime-main.82c27143.js"
+  },
+  {
+    "revision": "6ab06474363de872c2390ef25555b902",
+    "url": "./wp-content/plugins/quickpick/static/media/house.6ab06474.png"
+  },
+  {
+    "revision": "7af2baf74d484bb6cfc94e2827787fbb",
+    "url": "./wp-content/plugins/quickpick/static/media/single-item.7af2baf7.png"
+  },
+  {
+    "revision": "297881c25b147c5965fa10173436d09f",
+    "url": "./wp-content/plugins/quickpick/static/media/three-bedroom.297881c2.png"
+  },
+  {
+    "revision": "fbb5e2249936221ab1618a81cf05a5da",
+    "url": "./wp-content/plugins/quickpick/static/media/two-bedroom.fbb5e224.png"
+  }
+]);
